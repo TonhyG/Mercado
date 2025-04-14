0d8996ceed3f296ba19b199d27c79ed2d884c300
Anthony Guilherme <anthonymachadoliveira05@gmail.com>
2025-04-14 13:29:09 -0300

    Inicio

---
+++ Dia a Dia/src/Estrutura.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Estrutura {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        LocalDate dataAtual = LocalDate.now(); // Obtém a data atual
+
+        System.out.println("Data atual: " + dataAtual);
+        System.out.println("");
+
+    }
+
+}
